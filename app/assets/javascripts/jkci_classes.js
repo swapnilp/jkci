3fa239ed396278814a58e191d105e35e5dd7a498
SwapnilPatil <swapnil.patil04@gmail.com>
2014-10-03 12:28:23 +0530

    changes

--- app/assets/javascripts/jkci_classes.js
+++ app/assets/javascripts/jkci_classes.js
@@ -4,11 +4,13 @@ var selectedStudents = []
 function assignClassStudents(event, self){
     event.preventDefault();
     selectedStudents = [];
-    id = $(self).data("id");
-    $(".assignedStudents input[type=checkbox]:checked").each(function(){
-	selectedStudents.push($(this).attr("id"));
-    })
-    $.post("/class/"+id +"/manage_students.json", {students_ids: selectedStudents}, function(data){
-	window.location = "/jkci_classes/"+ data.id;
-    })	
+    if (window.confirm("Are you sure?")) {
+	id = $(self).data("id");
+	$(".assignedStudents input[type=checkbox]:checked").each(function(){
+	    selectedStudents.push($(this).attr("id"));
+	})
+	    $.post("/class/"+id +"/manage_students.json", {students_ids: selectedStudents}, function(data){
+		window.location = "/jkci_classes/"+ data.id;
+	    })	
+    }
 }
