be7265d7c11b1b35bd613072ebf17a92b28afd52
SwapnilPatil <swapnil.patil04@gmail.com>
2014-10-04 19:56:09 +0530

    worked on class exam

--- app/assets/javascripts/jkci_classes.js
+++ app/assets/javascripts/jkci_classes.js
@@ -1,5 +1,6 @@
 
-var selectedStudents = []
+var selectedStudents = [];
+var selectedPoints = [];
 
 function assignClassStudents(event, self){
     event.preventDefault();
@@ -12,5 +13,17 @@ function assignClassStudents(event, self){
 	    $.post("/class/"+id +"/manage_students.json", {students_ids: selectedStudents}, function(data){
 		window.location = "/jkci_classes/"+ data.id;
 	    })	
+    }
+}
+
+function createClassExam(event, self){
+    event.preventDefault();
+    selectedPoints = [];
+    if (window.confirm("Are you sure?")) {
+	id = $(self).data("id");
+	$(".dailyTeach input[type=checkbox]:checked").each(function(){
+	    selectedPoints.push($(this).attr("id"));
+	})
+	    window.location = "/exams/new?&jkci_class_id="+ id +"&dtp="+ selectedPoints;
     }
 }
