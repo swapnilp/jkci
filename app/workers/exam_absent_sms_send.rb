8ccc9a1448d71a77637cf0a6fdecf38832bb64b8
SwapnilPatil <swapnil.patil04@gmail.com>
2014-10-09 17:44:58 +0530

    worked on UI

--- app/workers/exam_absent_sms_send.rb
+++ app/workers/exam_absent_sms_send.rb
@@ -6,7 +6,7 @@ class ExamAbsentSmsSend < Struct.new(:exam)
   def send_sms(exam)
     exam.exam_catlogs.includes([:student]).only_absents.in_groups_of(50).each_with_index do |exam_catlog_group, index|
       message = "Your child is not attended #{exam.name} exam. Please contact with class "
-      #"https://www.txtguru.in/imobile/api.php?username=swapnilpatil04&password=95183992&source=Senderid&dmobile=#{exam_catlog_group.compact.map(&:student).map(&:p_mobile).join(',')}&message=#{message}"
+      #"https://www.txtguru.in/imobile/api.php?username=swapnilpatil04&password=95183992&source=update&dmobile=#{exam_catlog_group.compact.map(&:student).map(&:p_mobile).join(',')}&message=#{message}"
       SmsSent.new({number: index, obj_type: "absent_exam", obj_id: exam.id, message: message, is_parent: true}).save
     end
   end
--- app/workers/exam_result_sms_send.rb
+++ app/workers/exam_result_sms_send.rb
@@ -6,7 +6,7 @@ class ExamResultSmsSend < Struct.new(:exam)
   def send_sms(exam)
     exam.exam_catlogs.includes([:student]).only_results.each_with_index do |exam_catlog, index|
       message = "Your child got  #{exam_catlog.marks} out of #{exam.marks} in #{exam.name} exam. Please call us for further details"
-      #"https://www.txtguru.in/imobile/api.php?username=swapnilpatil04&password=95183992&source=Senderid&dmobile=#{exam_catlog.student.p_mobile}&message=#{message}"
+      #"https://www.txtguru.in/imobile/api.php?username=swapnilpatil04&password=95183992&source=update&dmobile=#{exam_catlog.student.p_mobile}&message=#{message}"
       SmsSent.new({number: exam_catlog.student.p_mobile, obj_type: "exam result", obj_id: exam_catlog.id, message: message, is_parent: true}).save
     end
   end
