ec395b006fda990c2b3a67ca7cad97d294526c9c
SwapnilPatil <swapnil.patil04@gmail.com>
2015-01-03 14:03:35 +0530

    worked on utilities

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+class SessionsController < Devise::SessionsController
+  skip_before_filter :verify_authenticity_token, only: [:create, :destroy]  
+  #after_filter :set_csrf_headers, only: [:create, :destroy]
+
+  protected
+  def set_csrf_headers
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?  
+  end
+end
