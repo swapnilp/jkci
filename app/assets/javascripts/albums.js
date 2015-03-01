c27fd5d51adae7544048ecb10ccb64093127ec74
SwapnilPatil <swapnil.patil04@gmail.com>
2015-03-01 22:59:43 +0530

    worked on UI

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -2,28 +2,29 @@
 
 
 function addGalleryImage(self){
-    $('.seek_img_spinner').show();
-    var file_path = $(self).val()
-    var thsEle = $(self);
-    if(file_path != ''){
-      $('#new_gallery').ajaxSubmit(function(data){
-       if(data.success){
-         $('.seek_img_spinner').hide();
-         $(".seekImgName").html(data['name']);
-           //$("#seek_image_url").val($("#seek_image_url").val() + data['image_path'] +",");
-         $(".imageUplaodBoxContainer").prepend(""+ data['html']);
-         $(".uploadSeekImgBtnBox").hide();
-         $(".imageUplaodBox").show();
-         $(".seekImgHolder").css("background", "#FFFFFF");
-         
-       }else{
-         brandiktivAlert(""+ data.msg);
-         $('.seek_img_spinner').hide();
-       }
-      });
-    }
-
+  $('.seek_img_spinner').show();
+  var file_path = $(self).val()
+  var thsEle = $(self);
+  if(file_path != ''){
+    $(self).parent().find('.loadingImg').removeClass('hide');
+    $('#new_gallery').ajaxSubmit(function(data){
+      if(data.success){
+        $(self).parent().find('.loadingImg').hide();
+        $(".seekImgName").html(data['name']);
+        //$("#seek_image_url").val($("#seek_image_url").val() + data['image_path'] +",");
+        $(".imageUplaodBoxContainer").prepend(""+ data['html']);
+        $(".uploadSeekImgBtnBox").hide();
+        $(".imageUplaodBox").show();
+        $(".seekImgHolder").css("background", "#FFFFFF");
+	$(".loadingImg").addClass('hide');
+      }else{
+        brandiktivAlert(""+ data.msg);
+	$(self).parent().find('.loadingImg').addClass('hide');
+      }
+    });
   }
+  
+}
 
 function destroyAlbumImage(self) {
     var id = $(self).parent().attr('data-id');
